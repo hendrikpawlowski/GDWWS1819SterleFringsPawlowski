caca84f77c84d669bedac978448d9dd007a5382c
hendrikhaselnuss <pawlowskihendrik@googlemail.com>
2018-12-10 15:10:48 +0100

    Kunde und Einkaufsliste als Objekte eingefuegt

--- api/resources/kunde.js
+++ api/resources/kunde.js
@@ -1,16 +1,31 @@
-const express = require("express")
-const router = express.Router()
-const fs = require("fs")
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+const app = require('../../app');
+let kundelol = app.getKunde();
+
+
+
+
+
 
 router.get("/", (req, res, next) => {
-    res.status(200).send("get bitches")
+
+    res.status(200).json({
+        id: kundelol.id,
+        benutzername: kundelol.benutzername,
+        passwort: kundelol.passwort
+    });
 })
+
 
 router.get("/:kundeID", (req, res, next) => {
-    const id = req.params.kundeID
+    const id = req.params.kundeID;
 
     if(id == 123)
-    res.status(200).send("verpiss dich")
+    res.status(200).send("GET Request on /kunde/123");
 })
+
 
 module.exports = router;
--- app.js
+++ app.js
@@ -3,6 +3,30 @@ const app = express();
 
 const kundeRoutes = require('./api/resources/kunde');
 
+
+
+
+const Kunde = function(id, benutzername, passwort) {
+    this.id = id;
+    this.benutzername = benutzername;
+    this.passwort = passwort;
+    let einkaufsliste = new Array();
+}
+
+
+const Einkaufsliste = function(name) {
+    this.name = name;
+    let produkte = new Array();
+}
+
+var kunde1 = new Kunde(1234, "hendrik", "pass");
+
+module.exports = {
+    getKunde: function() {
+        return kunde1;
+    }
+}
+
 app.use('/kunde', kundeRoutes);
 
 module.exports = app;
