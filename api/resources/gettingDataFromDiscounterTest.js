11aa7519e536a7ade6f824ad0d23f4a114b8089c
Hendrik Pawl <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-01-10 19:56:05 +0100

    Testen einer Discounter API

---
+++ api/resources/gettingDataFromDiscounterTest.js
@@ -0,0 +1,14 @@
+var request = require("request");
+
+// Discounter in USA: var url = "https://api.discountapi.com/v2/deals?api_key=YOUR_API_KEY=lOESXybs";
+
+request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+
+        console.log(body); // Print the json response
+    }
+})
